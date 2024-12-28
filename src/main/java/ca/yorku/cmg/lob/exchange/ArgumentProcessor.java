a067ae77fed418b7b6e0c6946bd018525c4266ac
liaskos <liaskos@yorku.ca>
2024-12-27 19:04:55 -0500

    Latest updates

--- src/main/java/ca/yorku/cmg/lob/exchange/ArgumentProcessor.java
+++ src/main/java/ca/yorku/cmg/lob/exchange/ArgumentProcessor.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) Sotirios Liaskos (liaskos@yorku.ca) - All Rights Reserved
+ * 
+ * This source code is protected under international copyright law.  All rights
+ * reserved and protected by the copyright holder.
+ * This file is confidential and only available to authorized individuals with the
+ * permission of the copyright holder.  If you encounter this file and do not have
+ * permission, please contact the copyright holder and delete this file.
+ */
 package ca.yorku.cmg.lob.exchange;
 
 import java.io.File;
